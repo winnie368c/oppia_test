e54f63978f1b05d400f76f6c489332fa0ce3dada
Winnie Chen <winnie@DNa808270.SUNet>
2023-04-23 19:40:00 -0700

    c

--- core/domain/action_registry_test.py
+++ core/domain/action_registry_test.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for methods in the action registry. 3"""
+"""Tests for methods in the action registry. 4"""
 
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
