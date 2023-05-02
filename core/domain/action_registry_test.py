2428434b5ce9197b858f1c493b2f542f023ff3f7
Winnie Chen <winnie@DN0a221240.SUNet>
2023-05-02 14:33:18 -0700

    c

--- core/domain/action_registry_test.py
+++ core/domain/action_registry_test.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for methods in the action re."""
+"""Tests for methods in the action ree."""
 
 from __future__ import annotations
 
