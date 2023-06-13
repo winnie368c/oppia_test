3504ceb8feec8f3b54230bbb96f40830cec4de44
Winnie Chen <winnie@DNa808270.SUNet>
2023-06-13 13:34:40 -0700

    mc

--- core/domain/action_registry.py
+++ core/domain/action_registry.py
@@ -35,7 +35,7 @@ if MYPY: # pragma: no cover
 
 
 class Registry:
-    """Registry of all actions5."""
+    """Registry of all actions53."""
 
     # Dict mapping action IDs to instances of the actions.
     _actions: Dict[str, base.BaseLearnerActionSpec] = {}
